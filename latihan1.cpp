68df6e3d4c7976460fa36ebb108ce2adbe2eeb7e
Rangga857 <azharrangga28@gmail.com>
2023-03-06 12:16:01 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   // begin
+   // Numeric ls, p, l
+   double ls, p, l;
+   // display 'masukan panjang'
+   cout << "masukan panjang = ";
+   // Accept p
+   cin >> p;
+   // display 'masukan lebar'
+   cout << "masukan lebar = ";
+   // Accept l
+   cin >> l;
+   // Compute ls = p*l
+   ls = p*l;
+   // display " luasnya = "+ls
+   cout << "Luasnya = "<< ls;
+   // end
+    
+
+
+   
+} 
